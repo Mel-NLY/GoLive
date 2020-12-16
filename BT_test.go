b70b1b30b4659ee7def1c5e2cbf0badff961d034
Li Yun Melissa Ng <s10177744@connect.np.edu.sg>
2020-12-16 17:56:28 +0800

    added go.sum and go.mod

--- BT_test.go
+++ BT_test.go
@@ -16,6 +16,6 @@ func TestDB(t *testing.T) {
 	}
 	mutex.Unlock()
 	if testUser.First != "TestFirstName" || testUser.Last != "TestLastName" || bcrypt.CompareHashAndPassword(testUser.Password, []byte("TestPassword")) != nil || testUser.Email != "testuser@test.com" {
-		t.Errorf("GetUser has obtained incorrect results, got: %v, want: %v.", testUser, User{"testUser", []byte("TestPassword"),"TestFirstName", "TestLastName", "testuser@test.com"})
+		t.Errorf("GetUser has obtained incorrect results, got: %v, want: %v.", testUser, User{"testUser", []byte("TestPassword"), "TestFirstName", "TestLastName", "testuser@test.com"})
 	}
 }
---
+++ go.mod
@@ -0,0 +1,15 @@
+module GoLive
+
+go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/xconstruct/go-pushbullet v0.0.0-20171206132031-67759df45fbb
+	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/mail.v2 v2.3.1
+)
